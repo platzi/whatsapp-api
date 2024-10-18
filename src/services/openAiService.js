07d3976533eb2ab1b33e2df465b967e6aef86783
Oscar Barajas Tavares <gndx@MacBook-Pro-de-Oscar.local>
2024-10-18 10:40:01 -0500

    [ADD] openAi

---
+++ src/services/openAiService.js
@@ -0,0 +1,20 @@
+import OpenAI from "openai";
+import config from "../config/env.js";
+
+const client = new OpenAI({
+    apiKey: config.CHATGPT_API_KEY,
+});
+
+const openAiService = async (message) => {
+    try {
+        const response = await client.chat.completions.create({
+            messages: [{ role: 'system', content: 'Comportarte como un veterinario, deberas de resolver las preguntas lo más simple posible. Responde en texto plano, como si fuera una conversacio por whatsapp, no saludes, no generas conversaciones, solo respondes con la pregunta del usuario.'}, { role: 'user', content: message }],
+            model: 'gpt-4o'
+        });
+        return response.choices[0].message.content;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export default openAiService;
